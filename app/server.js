475f63e0efd60c6229321cb1572f8dc1137a2b75
mhkeller <code@mhkeller.com>
2018-08-05 14:02:59 -0400

    add basename

--- app/server.js
+++ app/server.js
@@ -6,6 +6,7 @@ import { manifest } from './manifest/server.js';
 
 polka() // You can also use Express
 	.use(
+    '/layercake-examples',
 		compression({ threshold: 0 }),
 		sirv('assets'),
 		sapper({ manifest })
