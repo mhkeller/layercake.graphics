34da0d43c5412260965ffe4bc0302d0675fd6c4f
mhkeller <code@mhkeller.com>
2018-08-05 13:49:44 -0400

    reorganize

---
+++ routes/_examples.js
@@ -0,0 +1,7 @@
+import Line from '../components/examples/Line.html';
+import ScatterCanvas from '../components/examples/ScatterCanvas.html';
+
+export default [
+	{ title: 'Line + area', slug: 'Line', component: Line },
+	{ title: 'Scatter (canvas)', slug: 'ScatterCanvas', component: ScatterCanvas },
+];
