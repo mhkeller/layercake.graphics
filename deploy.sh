5310d7c03025fd4abc7fc468f27d42cbd6d95a62
mhkeller <code@mhkeller.com>
2018-08-05 19:43:02 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+cd export/layercake-examples && git init
+git add -A
+git commit -am 'init'
+git remote add origin https://github.com/mhkeller/layercake-examples.git
+git push origin master:gh-pages -f
