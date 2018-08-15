a810d5b0a8cb62afb61e5e25a900c80738e75097
mhkeller <code@mhkeller.com>
2018-08-15 22:30:24 +0200

    add map example

--- routes/_examples.js
+++ routes/_examples.js
@@ -1,7 +1,9 @@
 import Line from '../components/examples/Line.html';
 import ScatterCanvas from '../components/examples/ScatterCanvas.html';
+import MapSvg from '../components/examples/MapSvg.html';
 
 export default [
 	{ title: 'Line + area', slug: 'Line', component: Line },
-	{ title: 'Scatter (svg + canvas)', slug: 'ScatterCanvas', component: ScatterCanvas },
+  { title: 'Scatter (svg + canvas)', slug: 'ScatterCanvas', component: ScatterCanvas },
+	{ title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
 ];
