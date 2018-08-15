ac2484623fe21af07888e85f044d52f867c253a0
mhkeller <code@mhkeller.com>
2018-08-15 23:41:57 +0200

    add column chart

---
+++ data/groups.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    year: '1979',
+    value: 8
+  },
+  {
+    year: '1980',
+    value: 28
+  },
+  {
+    year: '1981',
+    value: 30
+  },
+  {
+    year: '1982',
+    value: 17
+  },
+  {
+    year: '1983',
+    value: 18
+  }
+];
--- routes/_examples.js
+++ routes/_examples.js
@@ -1,9 +1,11 @@
 import Line from '../components/examples/Line.html';
 import ScatterCanvas from '../components/examples/ScatterCanvas.html';
 import MapSvg from '../components/examples/MapSvg.html';
+import Column from '../components/examples/Column.html';
 
 export default [
 	{ title: 'Line + area', slug: 'Line', component: Line },
   { title: 'Scatter (svg + canvas)', slug: 'ScatterCanvas', component: ScatterCanvas },
-	{ title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
+  { title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
+	{ title: 'Column', slug: 'Column', component: Column },
 ];
