da840e0dfe9bc5ca8b7c2d9c0d4e4cb3faeea07b
mhkeller <code@mhkeller.com>
2018-08-18 16:55:14 +0200

    add linting; better code whitespace handling

--- routes/examples/[slug].json.js
+++ routes/examples/[slug].json.js
@@ -1,51 +1,56 @@
 import fs from 'fs';
 
 function cleanMain (example) {
-  return example.split('export')[1]
-    .replace(/(}\n}\n<\/script>| default.*|oncreate.*|^\s{2})/gm, '')
-    .replace(/\t/g, '  ')
-    .replace('this.refs.chart', 'document.getElementById(\'my-chart\')')
-    .trim();
+	const js = example.split('<script>')[1];
+	const parts = js.split('export');
+
+	const imports = parts[0].trim();
+	const oncreate = parts[1].trim()
+		.replace(/(}\n};\n<\/script>|default {.*|oncreate.*)/g, '')
+		.replace('this.refs.chart', 'document.getElementById(\'my-chart\')')
+		.split('\n').map(d => d.replace(/^\t\t/m, '').replace(/\t/g, '  ')).join('\n')
+		.trim();
+	return [imports, oncreate].join('\n\n');
 }
 
 function getComponentPaths (example) {
-  return example.match(/\.\.\/.+html/gm)
-    .map(d => d.replace('../', ''))
+	return example.match(/\.\.\/.+html/gm)
+		.map(d => d.replace('../', ''));
 }
 
-export function get(req, res, next) {
-  const { slug } = req.params;
+export function get (req, res, next) {
+	const { slug } = req.params;
 
-  const examplePath = `components/examples/${slug}.html`;
+	const examplePath = `components/examples/${slug}.html`;
 
-  if (!fs.existsSync(examplePath)) {
-    res.writeHead(404, {
-      'Content-Type': 'application/json'
-    });
+	if (!fs.existsSync(examplePath)) {
+		res.writeHead(404, {
+			'Content-Type': 'application/json'
+		});
 
-    res.end(JSON.stringify({
-      message: `Not found: ${slug}`
-    }));
-    return;
-  }
+		res.end(JSON.stringify({
+			message: `Not found: ${slug}`
+		}));
+		return;
+	}
 
-  const example = fs.readFileSync(examplePath, 'utf-8');
-  const main = { 
-    title: 'main.js', 
-    contents: cleanMain(example) 
-  };
+	const example = fs.readFileSync(examplePath, 'utf-8');
+	const main = {
+		title: 'main.js',
+		contents: cleanMain(example)
+	};
 
-  const components = getComponentPaths(example)
-    .map(d => {
-      return {
-        title: d,
-        contents: fs.readFileSync(`components/${d}`, 'utf-8').replace(/\t/g, '  ').trim()
-      }
-    });
+	const components = getComponentPaths(example)
+		.map(d => {
+			return {
+				title: d,
+				contents: fs.readFileSync(`components/${d}`, 'utf-8').replace(/\t/g, '  ').trim()
+			};
+		});
 
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
 
-  res.end(JSON.stringify({ pages: [main].concat(components) }));
+	res.end(JSON.stringify({ pages: [main].concat(components) }));
 }
