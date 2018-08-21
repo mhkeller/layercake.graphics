80f0ad4182e546353c2da1cd7307515bba6faab8
mhkeller <code@mhkeller.com>
2018-08-20 22:17:29 -0400

    add stacked example

--- routes/_examples.js
+++ routes/_examples.js
@@ -2,10 +2,12 @@ import Line from '../components/examples/Line.html';
 import ScatterCanvas from '../components/examples/ScatterCanvas.html';
 import MapSvg from '../components/examples/MapSvg.html';
 import Column from '../components/examples/Column.html';
+import AreaStacked from '../components/examples/AreaStacked.html';
 
 export default [
 	{ title: 'Line + area', slug: 'Line', component: Line },
-  { title: 'Scatter (svg + canvas)', slug: 'ScatterCanvas', component: ScatterCanvas },
-  { title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
+	{ title: 'Scatter (svg + canvas)', slug: 'ScatterCanvas', component: ScatterCanvas },
+	{ title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
 	{ title: 'Column (with annotation)', slug: 'Column', component: Column },
+	{ title: 'Stacked area', slug: 'AreaStacked', component: AreaStacked },
 ];
