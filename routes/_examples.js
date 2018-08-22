5dc788bb1440e990f2b239af5d21478ba92fd0a4
mhkeller <code@mhkeller.com>
2018-08-21 22:39:11 -0400

    stacked column

--- routes/_examples.js
+++ routes/_examples.js
@@ -3,6 +3,7 @@ import ScatterCanvas from '../components/examples/ScatterCanvas.html';
 import MapSvg from '../components/examples/MapSvg.html';
 import Column from '../components/examples/Column.html';
 import AreaStacked from '../components/examples/AreaStacked.html';
+import ColumnStacked from '../components/examples/ColumnStacked.html';
 
 export default [
 	{ title: 'Line + area', slug: 'Line', component: Line },
@@ -10,4 +11,5 @@ export default [
 	{ title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
 	{ title: 'Column (with annotation)', slug: 'Column', component: Column },
 	{ title: 'Stacked area', slug: 'AreaStacked', component: AreaStacked },
+	{ title: 'Stacked column', slug: 'ColumnStacked', component: ColumnStacked },
 ];
