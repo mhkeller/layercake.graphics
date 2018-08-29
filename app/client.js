e90c255f3f432d4afbe53471109d6e2818c43a78
mhkeller <code@mhkeller.com>
2018-08-28 23:20:43 -0400

    WIP more examples

--- app/client.js
+++ app/client.js
@@ -2,10 +2,17 @@ import { init } from 'sapper/runtime.js';
 import { Store } from 'svelte/store.js';
 import { manifest } from './manifest/client.js';
 
-const store = new Store({ activeGuideSection: null });
+const store = new Store({
+	activeGuideSection: ''
+});
 
 init({
 	target: document.querySelector('#sapper'),
 	manifest,
-	// store
+	store: data => {
+		store.set(data);
+		window.store = store;
+
+		return store;
+	}
 });
