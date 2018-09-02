50177f3b50252b22c5f04c42ce64e61d25e30003
mhkeller <code@mhkeller.com>
2018-09-01 23:05:44 -0400

    horizontal bar, x axis improvements

--- routes/_examples.js
+++ routes/_examples.js
@@ -6,14 +6,16 @@ import AreaStacked from '../components/examples/AreaStacked.html';
 import ColumnStacked from '../components/examples/ColumnStacked.html';
 import MultiLine from '../components/examples/MultiLine.html';
 import MapLayered from '../components/examples/MapLayered.html';
+import Bar from '../components/examples/Bar.html';
 
 export default [
 	{ title: 'Line + area', slug: 'Line', component: Line },
 	{ title: 'Scatter (svg + canvas + voronoi)', slug: 'ScatterCanvas', component: ScatterCanvas },
 	{ title: 'Map (svg)', slug: 'MapSvg', component: MapSvg },
+	{ title: 'Multilayer map (canvas + svg)', slug: 'MapLayered', component: MapLayered },
 	{ title: 'Column (with annotation)', slug: 'Column', component: Column },
 	{ title: 'Stacked area', slug: 'AreaStacked', component: AreaStacked },
 	{ title: 'Stacked column', slug: 'ColumnStacked', component: ColumnStacked },
 	{ title: 'Multiline (html labels + quadtree tooltip)', slug: 'MultiLine', component: MultiLine },
-	{ title: 'Multilayer map (canvas + svg)', slug: 'MapLayered', component: MapLayered },
+	{ title: 'Bar', slug: 'Bar', component: Bar },
 ];
