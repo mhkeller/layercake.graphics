933f969f8e33e13b82b3c9b8ef4579357d3414d6
mhkeller <code@mhkeller.com>
2019-12-28 13:28:13 -0800

    scaffold webgl example

---
+++ src/routes/_examples/ScatterWebgl.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { LayerCake, Svg } from 'layercake';
+	import points from '../../data/points.js';
+	// import ScatterWebgl from '../../components/ScatterWebgl.html';
+	import AxisX from '../../components/AxisX.svelte';
+	import AxisY from '../../components/AxisY.svelte';
+	// import QuadTree from '../../components/QuadTree.html';
+
+	const diameter = 6;
+	const padding = 6;
+
+	// .webglLayers([
+	// 	{ component: ScatterWebgl, opts: { diameter } }
+	// ])
+	// .htmlLayers([
+	// 	{ component: QuadTree, opts: { color: '#fff' } }
+	// ]);
+</script>
+
+<div class="chart-container">
+	<LayerCake
+		padding={{ top: 0, right: 5, bottom: 20, left: 25 }}
+		x={'myX'}
+		y={'myY'}
+		xPadding={[padding, padding]}
+		yPadding={[padding, padding]}
+		data={points}
+	>
+		<Svg>
+			<AxisX/>
+			<AxisY/>
+		</Svg>
+	</LayerCake>
+</div>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
