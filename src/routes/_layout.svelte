1eb572aa10ec694324b6a3cf87e5f4f693e09b0d
mhkeller <code@mhkeller.com>
2020-01-09 23:57:52 -0500

    fix padding

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,14 +5,6 @@
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 	:global(.input-container) {
 		user-select: none;
 	}
