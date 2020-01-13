43cf53e3e278bd8e726bf798e419048411674a1f
mhkeller <code@mhkeller.com>
2020-01-13 00:09:37 -0500

    clean up

--- src/routes/example/[slug].json.js
+++ src/routes/example/[slug].json.js
@@ -89,22 +89,4 @@ export function get(req, res, next) {
 	});
 
 	res.end(JSON.stringify(response));
-
-	// const fromMain = cleanMain(example);
-
-	// if (lookup.has(slug)) {
-	// 	res.writeHead(200, {
-	// 		'Content-Type': 'application/json'
-	// 	});
-
-	// 	res.end(lookup.get(slug));
-	// } else {
-	// 	res.writeHead(404, {
-	// 		'Content-Type': 'application/json'
-	// 	});
-
-	// 	res.end(JSON.stringify({
-	// 		message: `Not found`
-	// 	}));
-	// }
 }
