3231a677798455a0885144de343a4038424f988d
mhkeller <code@mhkeller.com>
2020-01-14 16:00:47 -0500

    navigate by dropdown

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,20 +1,23 @@
 <script>
-	import { goto } from '@sapper/app';
+	import { goto, stores } from '@sapper/app';
 
 	import GuideContents from './GuideContents.svelte';
 	import examples from '../routes/_examples.js';
 
-	// export let page;
+	// export let path;
 	export let segment;
-	export let slug = '';
+	// export let slug = '';
+	let slug = '';
 	export let sections;
 
-	let page;
+	const { page } = stores();
+	$: path = $page.path;
+	$: slug = path.split('/').pop();
 
 	$: console.log('slug', slug);
-	$: console.log('page', page);
+	$: console.log('path', path);
 
-	// let basePath = '/';
+	let basePath = '/';
 	let open = false;
 
 	let nav;
@@ -22,14 +25,11 @@
 	const slimName = d => d.split(' (')[0];
 
 	function loadPage () {
-		console.log('loading page', page);
 		open = false;
-		if (page) {
-			goto(page);
-		}
+		goto(this.value || '/');
 	}
 
-	// $: (page, loadPage());
+	// $: (path, loadPage());
 
 	function toggleOpen () {
 		// if the menu is closing, scroll back to the top *after* it
@@ -307,7 +307,7 @@
 
 <ul class="dropdown">
 	<li>
-		<select on:input={loadPage} bind:value="{page}">
+		<select on:change={loadPage} value="{$page.path}">
 			<option selected="{segment === undefined}" value="">All</option>
 			{#each examples as example}
 				<option value="example/{example.slug}" selected="{slug === example.slug}">{slimName(example.title)}</option>
