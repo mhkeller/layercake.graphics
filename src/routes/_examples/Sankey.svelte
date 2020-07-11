73fcf3ed7b9b462e5cf23c7f25c99f53950c177e
mhkeller <code@mhkeller.com>
2020-07-11 15:23:28 -0400

    convert to tabs

--- src/routes/_examples/Sankey.svelte
+++ src/routes/_examples/Sankey.svelte
@@ -1,26 +1,26 @@
 <script>
-  import { LayerCake, Svg } from 'layercake';
+	import { LayerCake, Svg } from 'layercake';
 
-  import Sankey from '../../components/Sankey.svelte';
+	import Sankey from '../../components/Sankey.svelte';
 
 	import data from '../../data/sankey-data.js';
 </script>
 
 <style>
-  .chart-container {
-    width: 100%;
-    height: 100%;
-  }
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
 </style>
 
 <div class="chart-container">
-  <LayerCake
-	  {data}
-  >
-    <Svg>
-      <Sankey
+	<LayerCake
+		{data}
+	>
+		<Svg>
+			<Sankey
 				colorNodes={d => '#ab00d6'}
 			/>
-    </Svg>
-  </LayerCake>
+		</Svg>
+	</LayerCake>
 </div>
--- src/routes/_examples_ssr/Sankey.svelte
+++ src/routes/_examples_ssr/Sankey.svelte
@@ -1,30 +1,30 @@
 <script>
-  import { LayerCake, ScaledSvg } from 'layercake';
+	import { LayerCake, ScaledSvg } from 'layercake';
 
-  import Sankey from '../../components/Sankey.svelte';
+	import Sankey from '../../components/Sankey.svelte';
 
 	import data from '../../data/sankey-data.js';
 </script>
 
 <style>
-  .chart-container {
-    width: 100%;
-    height: 100%;
-  }
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
 </style>
 
 <div class="chart-container">
-  <LayerCake
-    ssr={true}
-    percentRange={true}
-	  {data}
-  >
-    <ScaledSvg>
-      <Sankey
-        nodeWidth={5 / 100}
-        nodePadding={10 / 100}
+	<LayerCake
+		ssr={true}
+		percentRange={true}
+		{data}
+	>
+		<ScaledSvg>
+			<Sankey
+				nodeWidth={5 / 100}
+				nodePadding={10 / 100}
 				colorNodes={d => '#ab00d6'}
 			/>
-    </ScaledSvg>
-  </LayerCake>
+		</ScaledSvg>
+	</LayerCake>
 </div>
