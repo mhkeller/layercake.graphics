619a6815f948f8a766ae1a8d31f4c3449e1b8a84
mhkeller <code@mhkeller.com>
2021-03-12 18:02:45 -0500

    add comments

--- src/routes/_examples/Beeswarm.svelte
+++ src/routes/_examples/Beeswarm.svelte
@@ -6,6 +6,7 @@
 	import AxisX from '../../components/AxisX.svelte';
 	import Beeswarm from '../../components/Beeswarm.svelte';
 
+	// This example loads csv data as json using @rollup/plugin-dsv
 	import data from '../../data/us-senate.csv';
 
 	const xKey = 'date_of_birth';
@@ -26,6 +27,12 @@
 </script>
 
 <style>
+	/*
+		The wrapper div needs to have an explicit width and height in CSS.
+		It can also be a flexbox child or CSS grid element.
+		The point being it needs dimensions since the <LayerCake> element will
+		expand to fill it.
+	*/
 	.chart-container {
 		width: 100%;
 		height: 100%;
