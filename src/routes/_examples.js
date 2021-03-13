69984d393fd0dea9408fbef2d47be44c5c6e7eed
mhkeller <code@mhkeller.com>
2021-03-12 19:22:40 -0500

    WIP force layout

--- src/routes/_examples.js
+++ src/routes/_examples.js
@@ -21,6 +21,7 @@ import Sankey from './_examples/Sankey.svelte';
 import Brush from './_examples/Brush.svelte';
 import SyncedBrush from './_examples/SyncedBrush.svelte';
 import Beeswarm from './_examples/Beeswarm.svelte';
+import ForceDirectedGraph from './_examples/ForceDirectedGraph.svelte';
 
 export default [
 	{ replPath: 'ad32f9f301484d0cace82272126e9d09?version=3.17.3', title: 'Line + area', slug: 'Line', component: Line },
@@ -46,5 +47,6 @@ export default [
 	{ replPath: '4e7a8affd4724d03b33493c85e50471c?version=3.30.0', title: 'Brush', slug: 'Brush', component: Brush },
 	{ replPath: '0eaf387a0a564971865fb6176f959940?version=3.30.0', title: 'Synced brush', slug: 'SyncedBrush', component: SyncedBrush },
 	{ replPath: '9d0e23f494f645b4a9623c46474462f6?version=3.30.0', title: 'Beeswarm', slug: 'Beeswarm', component: Beeswarm },
+	{ replPath: '', title: 'Force-directed graph', slug: 'ForceDirectedGraph', component: ForceDirectedGraph },
 ];
 
---
+++ src/routes/_examples/ForceDirectedGraph.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { LayerCake, Svg } from 'layercake';
+
+	import ForceDirectedGraph from '../../components/ForceDirectedGraph.svelte';
+
+	import data from '../../data/miserables.json';
+
+</script>
+
+<style>
+	/*
+		The wrapper div needs to have an explicit width and height in CSS.
+		It can also be a flexbox child or CSS grid element.
+		The point being it needs dimensions since the <LayerCake> element will
+		expand to fill it.
+	*/
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
+
+<div class="chart-container">
+	<LayerCake
+		data={data}
+	>
+		<Svg>
+			<ForceDirectedGraph/>
+		</Svg>
+	</LayerCake>
+</div>
