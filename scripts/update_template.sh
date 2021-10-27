ac7de68862817a220e8f9e3e415894b98580f9ab
mhkeller <code@mhkeller.com>
2021-10-27 16:07:53 -0400

    dont include all components

--- scripts/update_template.sh
+++ scripts/update_template.sh
@@ -9,6 +9,8 @@ rm -rf scripts/svelte-app/src/App.svelte
 rm -rf scripts/svelte-app/node_modules
 rm -rf scripts/svelte-app/src/data
 mkdir scripts/svelte-app/src/data
+rm -rf scripts/svelte-app/src/components
+mkdir scripts/svelte-app/src/components
 
 # build svelte-app.json
 node scripts/build-svelte-app-json.js `find scripts/svelte-app -type f`
