e7ca03e9d1cee576a522c79ed8cb2d729a7781c4
mhkeller <code@mhkeller.com>
2021-12-17 13:57:40 -0500

    improvements to the multiline example

--- src/routes/_examples/MultiLine.svelte
+++ src/routes/_examples/MultiLine.svelte
@@ -18,20 +18,22 @@
 	 */
 	const xKey = 'month';
 	const yKey = 'value';
-	const zKey = 'key';
+	const zKey = 'fruit';
 
 	const seriesNames = Object.keys(data[0]).filter(d => d !== xKey);
 	const seriesColors = ['#ffe4b8', '#ffb3c0', '#ff7ac7', '#ff00cc'];
 
 	const parseDate = timeParse('%Y-%m-%d');
 
+	/* --------------------------------------------
+	 * Create a "long" format that is a grouped series of data points
+	 */
 	const dataLong = seriesNames.map(key => {
 		return {
-			key,
+			[zKey]: key,
 			values: data.map(d => {
 				d[xKey] = typeof d[xKey] === 'string' ? parseDate(d[xKey]) : d[xKey]; // Conditional required for sapper
 				return {
-					key,
 					[yKey]: +d[key],
 					[xKey]: d[xKey]
 				};
@@ -39,8 +41,11 @@
 		};
 	});
 
-	// Make a flat array of the `values` of our nested series
-	// we can pluck the `value` field from each item in the array to measure extents
+	/* --------------------------------------------
+	 * Make a flat array of the `values` of our nested series
+	 * we can pluck the field set from `yKey` from each item
+	 * in the array to measure the full extents
+	 */
 	const flatten = data => data.reduce((memo, group) => {
 		return memo.concat(group.values);
 	}, []);
